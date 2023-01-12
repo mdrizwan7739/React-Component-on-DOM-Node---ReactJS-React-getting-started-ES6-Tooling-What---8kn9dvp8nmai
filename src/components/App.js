54cca4720bc762db1ddc8b2271184b93e8c75c84
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-01-12 16:39:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import '../styles/App.css';
 
 const App = () => {
 
-  let [count, setCount] = useState(0)
 
   return (
     <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
