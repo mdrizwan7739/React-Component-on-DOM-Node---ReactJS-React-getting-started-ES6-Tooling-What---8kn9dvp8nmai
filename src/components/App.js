67825e966d22c98b52f9ad2ba17e18b96e949618
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-01-12 16:36:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,16 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+
+import React, { useEffect, useState } from "react";
+import '../styles/App.css';
+
+const App = () => {
+
+  let [count, setCount] = useState(0)
+
+  return (
+    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+  )
+}
+
+
+export default App;
 
